f59e873827dcef84c7c8ec3ffe053a9a472aa00e
Ogaga <iyaraogaga69@gmail.com>
2022-12-28 08:53:35 +0100

    Stle navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import Categories from "./Categories";
+import { FaUser } from "react-icons/fa";
 import Home from "./Home";
+import styles from '../sass/_navbar.module.scss'
 
 const Navbar = (props) => {
   return (
     <>
-      <div>
-        <h1>Bookstore</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="Categories">Categories</Link>
-        </nav>
+      <div className={styles.navbar}>
+        <div className={styles['nav-link']}>
+          <h1 className={styles.heading}>Bookstore</h1>
+          <nav className={styles.nav}>
+            <Link className={styles.navlink} to="/">Home</Link>
+            <Link className={styles.navlink} to="Categories">Categories</Link>
+          </nav>
+        </div>
+        <div className={styles['nav-icon']}>
+                  <FaUser className={styles.icon} />
+        </div>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
