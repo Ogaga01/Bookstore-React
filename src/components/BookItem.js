1e705137217ef79df3850d12ae1a6314314ce986
Ogaga <iyaraogaga69@gmail.com>
2022-12-28 17:06:28 +0100

    Implement remove book logic

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import styles from '../sass/_bookitem.module.scss'
+import styles from "../sass/_bookitem.module.scss";
 import { bookActions } from "../store/book-slice";
 
 const BookItem = (props) => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const {category, title, author, id} = props.book
+  const { category, title, author, id } = props.book;
 
-    const removeBook = () => {
-        dispatch(bookActions.removeBook(id))
-    }
+  const removeBook = () => {
+    dispatch(bookActions.removeBook(id));
+  };
 
   return (
-    <li className={styles['book-li']}>
+    <li className={styles["book-li"]}>
       <div className={styles["about-book"]}>
         <div className={styles["book-info"]}>
-                  <h3 className={styles.category}>{category}</h3>
-                  <h2 className={styles.title}>{title}</h2>
-                  <p className={styles.author}>{author}</p>
+          <h3 className={styles.category}>{category}</h3>
+          <h2 className={styles.title}>{title}</h2>
+          <p className={styles.author}>{author}</p>
         </div>
         <div className={styles["book-settings"]}>
           <ul className={styles["settings-list"]}>
             <li className={styles["settings-li"]}>Comment</li>
-            <li className={styles["settings-li"]} onClick={removeBook}>Remove</li>
+            <li className={styles["settings-li"]} onClick={removeBook}>
+              Remove
+            </li>
             <li className={styles["settings-li"]}>Edit</li>
           </ul>
         </div>
@@ -38,12 +40,14 @@ const BookItem = (props) => {
           <h2 className={styles.percent}>50%</h2>
           <p className={styles.completed}>Completed</p>
         </div>
-          </div>
-          <div className={styles.line}></div>
+      </div>
+      <div className={styles.line}></div>
       <div className={styles["book-chapter"]}>
         <h3 className={styles.chapter}>Current chapter</h3>
         <h3 className={styles.chap}>Chapter 17</h3>
-        <button className={styles.update} type="button">Update Progress</button>
+        <button className={styles.update} type="button">
+          Update Progress
+        </button>
       </div>
     </li>
   );
